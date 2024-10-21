061db81e0fbc17b8807748cdeace47ab8a3e7869
Kirill <kirill0392@gmail.com>
2024-10-21 15:33:38 +0300

    chage tasks position in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ export type TasksType = {
 	isDone: boolean;
 };
 
-export const App = () => {
-	const tasks: TasksType[] = [
-		{ id: 1, name: "read", isDone: false },
-		{ id: 2, name: "eat", isDone: false },
-		{ id: 3, name: "sleep", isDone: false },
-		{ id: 4, name: "walk", isDone: false },
-		{ id: 5, name: "code", isDone: false },
-		{ id: 6, name: "drive", isDone: false },
-		{ id: 7, name: "work", isDone: false },
-	];
+const tasks: TasksType[] = [
+	{ id: 1, name: "read", isDone: false },
+	{ id: 2, name: "eat", isDone: false },
+	{ id: 3, name: "sleep", isDone: false },
+	{ id: 4, name: "walk", isDone: false },
+	{ id: 5, name: "code", isDone: false },
+	{ id: 6, name: "drive", isDone: false },
+	{ id: 7, name: "work", isDone: false },
+];
 
+export const App = () => {
 	return (
 		<div className="App">
 			<Todolist todolistTitle={"Tasks list:"} tasksArray={tasks} />
